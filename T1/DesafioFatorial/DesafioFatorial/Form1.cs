4c384d24507d50fb4e85a2345f5272b79a96da90
v009 <v009@benner.com.br>
2018-07-12 16:11:54 -0300

    Projeto de fatorial.

---
+++ T1/DesafioFatorial/DesafioFatorial/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioFatorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroFatorial = 1;
+            for (int n = 1; n <= 10; n++)
+            {
+                numeroFatorial = numeroFatorial * n;
+                MessageBox.Show("O fatorial de " + n + " é " + numeroFatorial);
+            }
+
+        }
+    }
+}
