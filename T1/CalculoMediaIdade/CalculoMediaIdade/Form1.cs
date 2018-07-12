349512a25c00106c9c3f32c1ad60fb2419e03d59
v009 <v009@benner.com.br>
2018-07-12 15:59:41 -0300

    Projeto cálculo da média de idades.

---
+++ T1/CalculoMediaIdade/CalculoMediaIdade/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoMediaIdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double idadeGuilherme = 18;
+            double idadeDylan = 22;
+            double idadeBruno = 17;
+            double idadeValter = 49;
+
+            double mediaIdade = ((idadeGuilherme + idadeDylan + idadeBruno + idadeValter) / 4);
+
+            MessageBox.Show("A média de idade é de " + mediaIdade + " anos");
+        }
+    }
+}
