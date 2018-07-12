21e4012a9627651d6e17a60f3f419a2103b0d27d
v009 <v009@benner.com.br>
2018-07-12 16:25:24 -0300

    Projeto de assunto passado na video-aula 8.

---
+++ T1/Aula8/Aula8/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula8
+{
+    class Conta
+    {
+        public int numero;
+        public double saldo;
+        public Cliente cliente;
+    }
+}
---
+++ T1/Aula8/Aula8/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta conta = new Conta();
+            Cliente cliente = new Cliente();
+            conta.cliente = cliente;
+
+            cliente.nome = "Guilherme";
+            cliente.endereco = "Rua Luíza Lucas";
+            cliente.rg = "5.726.289";
+
+            MessageBox.Show("O cliente " + conta.cliente.nome + " reside em " + conta.cliente.endereco + "," +
+                            "\ne é portador do Rg " + conta.cliente.rg);
+        }
+    }
+}
