ed570bb85ec770e11907fefd551237708e1d1725
v009 <v009@benner.com.br>
2018-07-12 16:23:59 -0300

    Projeto de assunto passado na video-aula 7.

---
+++ T1/Aula7/Aula7/Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7
+{
+    class Conta
+    {
+        public double saldo;
+        public int numero;
+        public string titular;
+
+        public void Saca(double valor)
+        {
+            if (valor >= 0 && saldo > valor)
+            {
+                this.saldo -= valor;
+            }
+        }
+
+        public void Deposita(double valor)
+        {
+            if (valor > 0)
+            {
+                this.saldo += valor;
+            }
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            /*this.saldo -= valor;
+            destino.saldo += valor*/
+
+            if (valor > 0)
+            {
+                this.Saca(valor);
+                destino.Deposita(valor);
+            }
+        }
+        public double CalculaRendimentoAnual()
+        {
+            double saldoNaqueleMes = this.saldo;
+
+            for (int i = 0; i < 12; i++)
+            {
+                saldoNaqueleMes = saldoNaqueleMes * 1.007;
+            }
+
+            double rendimento = saldoNaqueleMes - this.saldo;
+
+            return rendimento;
+        }
+
+    }
+}
---
+++ T1/Aula7/Aula7/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta Guilherme = new Conta();
+            Conta Mauricio = new Conta();
+            
+            Guilherme.saldo = 2000.0;
+            Mauricio.saldo = 1000.0;
+
+            Guilherme.Transfere(200.0, Mauricio);
+            MessageBox.Show("-> O saldo de Guilherme agora é de: R$ " + Guilherme.saldo);
+            MessageBox.Show("-> O saldo de Maurício agora é de: R$ " + Mauricio.saldo);
+
+            double rendimentoAnual = Mauricio.CalculaRendimentoAnual();
+            MessageBox.Show("O rendimento da conta de Maurício depois de 1 ano será: R$ " + rendimentoAnual + 
+                            "\nSaldo após 1 ano em conta: R$ " + (Mauricio.saldo+rendimentoAnual));
+        }
+    }
+}
