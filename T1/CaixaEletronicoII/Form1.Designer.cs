47e0e7e69b10651ab3c5ddb1a03825e1c1842fc0
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-07-13 23:58:48 -0300

    Inicialização da aplicação Caixa Eletrônico II de C# II

---
+++ T1/CaixaEletronicoII/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CaixaEletronicoII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTitular = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxTitular
+            // 
+            this.textBoxTitular.Location = new System.Drawing.Point(148, 259);
+            this.textBoxTitular.Name = "textBoxTitular";
+            this.textBoxTitular.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTitular.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxTitular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTitular;
+    }
+}
+
