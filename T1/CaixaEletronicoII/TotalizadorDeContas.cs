b29701a3047adb0aefbafe59f3fd370c063f6897
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-07-15 00:02:52 -0300

    Criação de class totalizadora de contas

---
+++ T1/CaixaEletronicoII/TotalizadorDeContas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicoII
+{
+    class TotalizadorDeContas
+    {
+        public double Total { get; private set; }
+
+        public void Adiciona(Conta conta)
+        {
+            this.Total += conta.Saldo; //toda vez que adiciona uma conta ao relatorio, pega o saldo da conta, e adiciona ao total.
+        }
+    }
+}
