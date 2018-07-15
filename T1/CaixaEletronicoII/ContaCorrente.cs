b193677286349204dea0b6e1dbb458741b895ed0
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-07-15 18:30:50 -0300

    Criação de método estático

--- T1/CaixaEletronicoII/ContaCorrente.cs
+++ T1/CaixaEletronicoII/ContaCorrente.cs
@@ -27,11 +27,13 @@ namespace CaixaEletronicoII
                 }
             }
         }
+        public static int TotalDeContasCorrentes { get; private set; } //vai contar quantas contas tem. Static diz que é da classe, e não um atributo de cada conta criada, assim vai contar quantas contas foram criadas no total.
 
         public ContaCorrente(double saldo, int Numero)
         {
             this.Saldo = saldo;
             this.numero = Numero;
+            ContaCorrente.TotalDeContasCorrentes++; //cada vez que criar uma conta corrente, ela vai somar no contador
         }
     }
 }
