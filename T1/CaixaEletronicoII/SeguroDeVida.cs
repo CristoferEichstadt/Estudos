a9715a6f9de5fc624b3efded9c6608a3e4ad0a41
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-07-15 18:17:25 -0300

    Criação de classe e interface

---
+++ T1/CaixaEletronicoII/SeguroDeVida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicoII
+{
+    class SeguroDeVida : ISeguroVida
+    {
+        public double CalculaImposto()
+        {
+            return 42.0;
+        }
+    }
+}
