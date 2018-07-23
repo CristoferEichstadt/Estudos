a7bfc2d80122e6f251f6e7019614cf8d81ed256c
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-07-22 23:26:06 -0300

    Implementação da class

--- T1/CaixaEletronicoII/StringUtils.cs
+++ T1/CaixaEletronicoII/StringUtils.cs
@@ -8,5 +8,16 @@ namespace CaixaEletronicoII
 {
     static class StringUtils 
     {
+        public static string Pluralize (string palavra)
+        {
+            if (palavra.EndsWith("s")) //se a palavra termina em S
+            {
+                return palavra;
+            }
+            else
+            {
+                return palavra + "s";
+            }
+        }
     }
 }
