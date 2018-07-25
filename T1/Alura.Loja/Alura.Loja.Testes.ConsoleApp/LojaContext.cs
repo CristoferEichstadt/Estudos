fdd2d3058e577bcda8885676e192e37967a70096
v009 <v009@benner.com.br>
2018-07-25 17:29:26 -0300

    Adicionando comandos do entity

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -5,10 +5,19 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     public class LojaContext : DbContext
     {
-        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Produto> Produtos { get; set; } //DbSet monitora a classe Produto e cria uma tabela automaticamente
+                                                     //a partir dos atributos da classe Produto, a tabela recebe o nome de Produtos;
+        public LojaContext() 
+        { }
+        public LojaContext(DbContextOptions<LojaContext> options) : base(options)
+        { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            }
         }
     }
 }
--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -8,27 +8,37 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) =>
             //GravarUsandoAdoNet();
-            GravarUsandoEntity();
+            //GravarUsandoEntity();
+            RecuperarProdutos();
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new LojaContext()) //faz o SELECT
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
         }
 
-        private static void GravarUsandoEntity()
+        private static void GravarUsandoEntity() //faz o INSERT
         {
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var contexto = new LojaContext())
+            using (var contexto = new LojaContext()) 
             {
                 contexto.Produtos.Add(p);
                 contexto.SaveChanges();
             }
-            
         }
-
         private static void GravarUsandoAdoNet()
         {
         }
