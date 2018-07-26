8c8285bcc078055bd58425ccde57eda0a65e7a8c
v009 <v009@benner.com.br>
2018-07-26 17:19:36 -0300

    Alteração em DAO e class produto

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -8,7 +8,16 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    Console.WriteLine(p);
+                }
+            }
         }
         
     }
