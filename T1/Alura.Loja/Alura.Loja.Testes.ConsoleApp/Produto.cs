ae9ab6c1826315b785b7284f0f0245a0107ef8af
v009 <v009@benner.com.br>
2018-07-26 17:28:32 -0300

    Alteração class produto

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,11 +5,11 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnitario { get; internal set; }
 
         public override string ToString()
         {
-            return $"Produto: {this.Id}, {this.Nome}, {this.Preco}"; 
+            return $"Produto: {this.Id}, {this.Nome}, {this.PrecoUnitario}"; 
         }
     }
 }
