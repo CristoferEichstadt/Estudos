cd9e9039fddaafde18b262812bb7edfee5413e30
v009 <v009@benner.com.br>
2018-07-27 17:19:51 -0300

    Criando class Promocao

---
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Promocao
+    {
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
