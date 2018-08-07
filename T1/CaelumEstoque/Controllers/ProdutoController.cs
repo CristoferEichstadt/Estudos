07c8a67ace3df7880f55064d01fbb00d84a87dc7
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-06 21:22:05 -0300

    Adaptação de formulário

--- T1/CaelumEstoque/Controllers/ProdutoController.cs
+++ T1/CaelumEstoque/Controllers/ProdutoController.cs
@@ -11,7 +11,7 @@ namespace CaelumEstoque.Controllers
     public class ProdutoController : Controller
     {
         // GET: Produto
-        public ActionResult Index() 
+        public ActionResult Index()
         {
             ProdutosDAO dao = new ProdutosDAO();
             IList<Produto> produtos = dao.Lista();//LISTA
@@ -24,20 +24,12 @@ namespace CaelumEstoque.Controllers
             return View();
         }
 
-        public ActionResult Adiciona(String nome, float preco, String descricao, int quantidade, int categoriaId)
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
         {
-            Produto produto = new Produto()
-            {
-                Nome = nome,
-                Preco = preco,
-                Descricao = descricao,
-                Quantidade = quantidade,
-                CategoriaId = categoriaId
-            };
-
             ProdutosDAO dao = new ProdutosDAO();
             dao.Adiciona(produto);
-            return View();
+            return RedirectToAction("Index", "Produto");
         }
     }
 }
