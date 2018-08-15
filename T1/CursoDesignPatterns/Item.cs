c8d6797a9874237ecae46b1a845b70bc5eab62c4
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-15 17:12:01 -0300

    Adicionando descontos

---
+++ T1/CursoDesignPatterns/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class Item
+    {
+        public String Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Item(String nome, double valor)
+        {
+            this.Nome = nome;
+            this.Valor = valor;
+        }
+    }
+}
--- T1/CursoDesignPatterns/Orcamento.cs
+++ T1/CursoDesignPatterns/Orcamento.cs
@@ -1,12 +1,21 @@
-﻿namespace CursoDesignPatterns
+﻿using System.Collections.Generic;
+
+namespace CursoDesignPatterns
 {
     public class Orcamento
     {
         public double Valor { get; private set; }
 
+        public IList<Item> Itens { get; private set; }
         public Orcamento(double valor)
         {
             this.Valor = valor;
+            this.Itens = new List<Item>();
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
         }
     }
 }
