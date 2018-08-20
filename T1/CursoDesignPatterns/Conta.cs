93619d69bdafdefffc144de5326a657205a8a3e3
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-20 08:32:13 -0300

    Inserindo propriedades em classe conta

--- T1/CursoDesignPatterns/Conta.cs
+++ T1/CursoDesignPatterns/Conta.cs
@@ -1,12 +1,19 @@
-﻿namespace CursoDesignPatterns
+﻿using System;
+
+namespace CursoDesignPatterns
 {
-    public class Conta 
+    public class Conta
     {
+        public String Titular { get; private set; }
         public double Saldo { get; private set; }
         public void Deposita(double valor)
         {
             this.Saldo += valor;
         }
+        public Conta(String titular, double saldo)
+        {
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
     }
 }
-
