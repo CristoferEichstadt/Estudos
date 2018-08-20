cb1775717f93cf07901092828d9e7dca5adfb536
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-20 09:48:56 -0300

    Criando relatorio simples

---
+++ T1/CursoDesignPatterns/RelatorioSimples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class RelatorioSimples : TemplateRelatorio
+    {
+        protected override void Cabecalho()
+        {
+            Console.WriteLine("Banco Itaú");
+        }
+
+        protected override void Rodape()
+        {
+            Console.WriteLine("(47) 3333-0000");
+        }
+
+        protected override void Corpo(IList<Conta> contas)
+        {
+            foreach (Conta c in contas)
+            {
+                Console.WriteLine(c.Titular + " -> " + c.Saldo);
+            }
+        }
+    }
+}
