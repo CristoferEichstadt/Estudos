4ffc797db1fa0d1cb643679442c7584c6c3c2ae4
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-20 09:53:00 -0300

    Criando relatorio completo e adicionando propriedades em conta

--- T1/CursoDesignPatterns/Conta.cs
+++ T1/CursoDesignPatterns/Conta.cs
@@ -5,15 +5,19 @@ namespace CursoDesignPatterns
     public class Conta
     {
         public String Titular { get; private set; }
+        public String Numero { get; set; }
+        public String Agencia { get; set; }
         public double Saldo { get; private set; }
         public void Deposita(double valor)
         {
             this.Saldo += valor;
         }
-        public Conta(String titular, double saldo)
+        public Conta(String titular, double saldo, String numero, String agencia)
         {
             this.Titular = titular;
             this.Saldo = saldo;
+            this.Numero = numero;
+            this.Agencia = agencia;
         }
     }
 }
