0b8da128514896dfaa4e960464df41fcc32d2a6d
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-20 09:39:43 -0300

    Finalização de template method

--- T1/CursoDesignPatterns/TemplateDeImpostoCondicional.cs
+++ T1/CursoDesignPatterns/TemplateDeImpostoCondicional.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace CursoDesignPatterns
 {
-    public class TemplateDeImpostoCondicional : IImposto
+    public abstract class TemplateDeImpostoCondicional : IImposto
     {
+        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
         public double Calcula(Orcamento orcamento)
         {
-            ;
+            if (DeveUsarMaximaTaxacao(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+
+            return MinimaTaxacao(orcamento);
         }
     }
 }
