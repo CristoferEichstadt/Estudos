6cba0d167d0c23e284158f787f3928acde5723e1
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-20 09:46:20 -0300

    Criando relatorio

--- T1/CursoDesignPatterns/IKCV.cs
+++ T1/CursoDesignPatterns/IKCV.cs
@@ -8,17 +8,17 @@ namespace CursoDesignPatterns
 {
     public class IKCV : TemplateDeImpostoCondicional
     {
-        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        protected override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor > 500 && TemItemMaiorQue100ReaisNo(orcamento);
         }
 
-        public override double MaximaTaxacao(Orcamento orcamento)
+        protected override double MaximaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor * 0.10;
         }
 
-        public override double MinimaTaxacao(Orcamento orcamento)
+        protected override double MinimaTaxacao(Orcamento orcamento)
         {
             return orcamento.Valor * 0.06;
         }
--- T1/CursoDesignPatterns/TemplateDeImpostoCondicional.cs
+++ T1/CursoDesignPatterns/TemplateDeImpostoCondicional.cs
@@ -8,9 +8,9 @@ namespace CursoDesignPatterns
 {
     public abstract class TemplateDeImpostoCondicional : IImposto
     {
-        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
-        public abstract double MaximaTaxacao(Orcamento orcamento);
-        public abstract double MinimaTaxacao(Orcamento orcamento);
+        protected abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+        protected abstract double MaximaTaxacao(Orcamento orcamento);
+        protected abstract double MinimaTaxacao(Orcamento orcamento);
         public double Calcula(Orcamento orcamento)
         {
             if (DeveUsarMaximaTaxacao(orcamento))
