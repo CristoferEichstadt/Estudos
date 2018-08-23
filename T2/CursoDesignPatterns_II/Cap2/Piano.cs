b0780f205cb6ac7700bf332d2abc374548292626
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-22 22:10:06 -0300

    Utilizando padrão de projeto Flyweight

---
+++ T2/CursoDesignPatterns_II/Cap2/Piano.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoDesignPatterns_II.Cap2
+{
+    public class Piano
+    {
+        public void Toca(IList<INota> musica)
+        {
+            foreach (var nota in musica)
+            {
+                Console.Beep(nota.Frequencia, 300);
+            }
+        }
+    }
+}
--- T2/CursoDesignPatterns_II/Program.cs
+++ T2/CursoDesignPatterns_II/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoDesignPatterns_II.Cap2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,19 @@ namespace CursoDesignPatterns_II
     {
         static void Main(string[] args)
         {
+            var notas = new NotasMusicais();
+            IList<INota> musica = new List<INota>()
+            {
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("mi"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+            };
 
+            var piano = new Piano();
+            piano.Toca(musica);
         }
     }
 }
