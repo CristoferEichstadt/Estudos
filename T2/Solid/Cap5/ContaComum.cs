716e2f2c1defc60328bee45412a8eec2a919040b
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-27 16:39:21 -0300

    Aplicando LSP; Composição;

--- T2/Solid/Cap5/ContaComum.cs
+++ T2/Solid/Cap5/ContaComum.cs
@@ -8,5 +8,24 @@ namespace Solid.Cap5
 {
     public class ContaComum
     {
+
+        private GerenciarSaldo Gerenciador { get; }
+        public ContaComum()
+        {
+            Gerenciador = new GerenciarSaldo();
+        }
+        public double Saldo { get; protected set; }
+        public void Deposita(double valor)
+        {
+            Gerenciador.Deposita(valor);
+        }
+        public void Saca(double valor)
+        {
+            Gerenciador.Saca(valor - 100);
+        }
+        public virtual void somaInvestimento()
+        {
+            Gerenciador.SomaInvestimento(1.1);
+        }
     }
 }
---
+++ T2/Solid/Cap5/ContaEstudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Cap5
+{
+    public class ContaEstudante : ContaComum
+    {
+        public int Milhas { get; protected set; }
+        private GerenciarSaldo Gerenciador;
+        public ContaEstudante()
+        {
+            Gerenciador = new GerenciarSaldo();
+        }
+        public void Deposita(double valor)
+        {
+            Gerenciador.Deposita(valor);
+            this.Milhas += (int)valor;
+        }
+    }
+}
