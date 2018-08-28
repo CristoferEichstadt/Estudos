ccd6ffbe3b361e886fa47f2bdf854160de6bcc7d
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-08-28 15:42:26 -0300

    Atividade: Verificar lances maiores que 5000

--- T2/Leilao.tests/AvaliadorTest.cs
+++ T2/Leilao.tests/AvaliadorTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Caelum.Leilao
 {
@@ -242,6 +243,52 @@ namespace Caelum.Leilao
         {
             MatematicaMaluca matematica = new MatematicaMaluca();
             Assert.AreEqual(5 * 2, matematica.ContaMaluca(5));
+        }
+
+        [Test]
+        public void DeveSelecionarLancesEntre1000E3000()
+        {
+            Usuario joao = new Usuario("Joao");
+
+            FiltroDeLances filtro = new FiltroDeLances();
+            IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+            new Lance(joao,2000),
+            new Lance(joao,1000),
+            new Lance(joao,3000),
+            new Lance(joao, 800)});
+
+            Assert.AreEqual(1, resultado.Count);
+            Assert.AreEqual(2000, resultado[0].Valor, 0.00001);
+        }
+
+        [Test]
+        public void DeveSelecionarLancesEntre500E700()
+        {
+            Usuario joao = new Usuario("Joao");
+
+            FiltroDeLances filtro = new FiltroDeLances();
+            IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+            new Lance(joao,600),
+            new Lance(joao,500),
+            new Lance(joao,700),
+            new Lance(joao, 800)});
+
+            Assert.AreEqual(1, resultado.Count);
+            Assert.AreEqual(600, resultado[0].Valor, 0.00001);
+        }
+
+        [Test]
+        public void DeveSelecionarLancesMaioresQue5000()
+        {
+            Usuario guilherme = new Usuario("Guilherme");
+
+            FiltroDeLances filtro = new FiltroDeLances();
+            IList<Lance> resultado = filtro.Filtra(new List<Lance>() {
+            new Lance(guilherme, 5023)});
+
+            Assert.AreEqual(1, resultado.Count);
+            Assert.AreEqual(5023, resultado[0].Valor, 0.0000001);
+
         }
     }
 }
