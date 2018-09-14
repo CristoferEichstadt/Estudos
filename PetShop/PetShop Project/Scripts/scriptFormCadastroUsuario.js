7cbae5e5d7f9f50664437662e36b24663d79d1b3
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-14 17:19:35 -0300

    Adição de cores

--- PetShop/PetShop Project/Scripts/scriptFormCadastroUsuario.js
+++ PetShop/PetShop Project/Scripts/scriptFormCadastroUsuario.js
@@ -43,6 +43,8 @@ $(document).ready(function mascaraIE() {
                 corNome.style.color = '#ff0000';
                 alert("Nome está incorreto!");
                 alert("Lembre-se, nome deve iniciar com letra maiúscula e deve ser completo!");
+            } else {
+                corNome.style.color = '#000000';
             }
         }
         //email
@@ -56,6 +58,8 @@ $(document).ready(function mascaraIE() {
                 event.preventDefault();
                 corEmail.style.color = '#ff0000';
                 alert("E-mail está incorreto!");
+            } else {
+                corEmail.style.color = '#000000';
             }
         }
 
@@ -72,6 +76,8 @@ $(document).ready(function mascaraIE() {
                 corSenha.style.color = '#ff0000';
                 alert("Senha está incorreta!");
                 alert('Lembre-se, a senha deve seguir o formato "Aaaa0000"!');
+            } else {
+                corSenha.style.color = '#000000';
             }
         }
 
@@ -80,6 +86,8 @@ $(document).ready(function mascaraIE() {
             event.preventDefault();
             corData.style.color = '#ff0000';
             alert("Preencha a data de nascimento!");
+        } else {
+            corData.style.color = '#000000';
         }
     });
 
@@ -101,6 +109,8 @@ $(document).ready(function mascaraIE() {
                 event.preventDefault();
                 corRSocial.style.color = '#ff0000';
                 alert("Razão Social está incorreta!");
+            } else {
+                corRSocial.style.color = '#000000';
             }
         }
 
@@ -115,6 +125,8 @@ $(document).ready(function mascaraIE() {
                 event.preventDefault();
                 corNomeFant.style.color = '#ff0000';
                 alert("Nome Fantasia está incorreto!");
+            } else {
+                corNomeFant.style.color = '#000000';
             }
         }
 
@@ -123,6 +135,15 @@ $(document).ready(function mascaraIE() {
             event.preventDefault();
             corIE.style.color = '#ff0000';
             alert("Preencha a inscrição estadual completa!");
+        } else {
+            var regexIe = new RegExp("^[0-9]{4,11}");
+            if (!regexIe.test($("#inscricaoEstadual").val())) {
+                event.preventDefault();
+                corIE.style.color = '#ff0000';
+                alert("Inscrição Estadual incorreta!");
+            } else {
+                corIE.style.color = '#000000';
+            }
         }
 
         //email
@@ -136,6 +157,8 @@ $(document).ready(function mascaraIE() {
                 event.preventDefault();
                 corEmailJuridica.style.color = '#ff0000';
                 alert("E-mail está incorreto!");
+            } else {
+                corEmailJuridica.style.color = '#000000';
             }
         }
 
@@ -151,6 +174,8 @@ $(document).ready(function mascaraIE() {
                 corSenhaJuridica.style.color = '#ff0000';
                 alert("Senha está incorreta!");
                 alert('Lembre-se, a senha deve seguir o formato "Aaaa0000"!');
+            } else {
+                corSenhaJuridica.style.color = '#000000';
             }
         }
 
