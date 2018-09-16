b4ad731abdba891c04b3ea53618e4488cb37d971
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-16 14:16:49 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- T3/introducao-javascript-inicial/introducao-javascript/js/Principal.js
+++ T3/introducao-javascript-inicial/introducao-javascript/js/Principal.js
@@ -1,10 +1,39 @@
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector(".info-peso");
-var tdAltura = document.querySelector(".info-altura");
+var pacientes = document.querySelectorAll(".paciente");
 
-var altura = tdAltura.textContent;
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    
+    var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
 
-console.log(imc);
+
+    var pesoEhValido = true;
+    var AlturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+    }
+
+    if(altura <= 0 || altura >= 3.00){
+        AlturaEhValida = false;
+        tdImc.textContent = "Altura Inválida!";
+    }
+
+
+
+    if(pesoEhValido && AlturaEhValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }else {
+        tdImc.textContent = "Altura e/ou peso inválidos!";
+    }
+
+}
