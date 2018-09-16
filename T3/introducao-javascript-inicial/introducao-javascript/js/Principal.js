fefb79e334c52de5d4b70ba0184ce6d891a72da6
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-16 12:26:21 -0300

    Iniciando com querySelector

---
+++ T3/introducao-javascript-inicial/introducao-javascript/js/Principal.js
@@ -0,0 +1,12 @@
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var tdAltura = document.querySelector(".info-altura");
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+console.log(paciente);
+console.log(tdPeso);
+
+console.log(altura);
+console.log(peso);
