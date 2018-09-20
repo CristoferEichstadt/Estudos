5f6f64b5ac69fecd0de8236335d8d73ff215c528
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-20 17:08:00 -0300

    Aula 4 - Criação de funções, atributos e reinicio de jogo;

--- T3/alura-typer-inicial/alura-typer/public/js/main.js
+++ T3/alura-typer-inicial/alura-typer/public/js/main.js
@@ -1,25 +1,59 @@
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function () {
-    var conteudo = campo.val();
 
-    var qtdPalavas = conteudo.split(/\S+/).length;
-    $("#contador-palavras").text(qtdPalavas);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase").text(numeroPalavras);
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function () {
 
-    var idCronometro = setInterval(function () {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(idCronometro);
-        }
-    }, 1000);
+function inicializaContadores(){
+    
+    campo.on("input", function () {
+        var conteudo = campo.val();
 
-});
+        var qtdPalavas = conteudo.split(/\S+/).length;
+        $("#contador-palavras").text(qtdPalavas);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+    
+}
+
+function inicializaCronometro(){
+    
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+
+        var idCronometro = setInterval(function () {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(idCronometro);
+            }
+        }, 1000);
+
+    });
+    
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
+
