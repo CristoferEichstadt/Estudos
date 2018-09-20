4bdc3233f8fdc7421a73c77baf8cb6a243f8f2f4
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-20 16:46:39 -0300

    Aula 3 - Game Over com eventos; Inserindo eventos de foco, cronometro etc

--- T3/alura-typer-inicial/alura-typer/public/js/main.js
+++ T3/alura-typer-inicial/alura-typer/public/js/main.js
@@ -1,16 +1,25 @@
-var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase").text(numeroPalavras);
-
-
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
+campo.on("input", function () {
     var conteudo = campo.val();
-    
+
     var qtdPalavas = conteudo.split(/\S+/).length;
     $("#contador-palavras").text(qtdPalavas);
-    
+
     var qtdCaracteres = conteudo.length;
     $("#contador-caracteres").text(qtdCaracteres);
+});
+
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function () {
+
+    var idCronometro = setInterval(function () {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(idCronometro);
+        }
+    }, 1000);
+
 });
