e92e297aeab1d65f50fb56e722a2cb5792af66d6
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-24 16:21:34 -0300

    Add-Migration

--- PetShop/PetShop Project/Migrations/20180924192013_ModificacaoModels.Designer.cs
+++ PetShop/PetShop Project/Migrations/20180924192013_ModificacaoModels.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetShop_Project.DAO;
 
 namespace PetShop_Project.Migrations
 {
     [DbContext(typeof(PetShopContext))]
-    partial class PetShopContextModelSnapshot : ModelSnapshot
+    [Migration("20180924192013_ModificacaoModels")]
+    partial class ModificacaoModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PetShop/PetShop Project/Migrations/20180924192013_ModificacaoModels.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetShop_Project.Migrations
+{
+    public partial class ModificacaoModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subcategorias_Categorias_CategoriaId",
+                table: "Subcategorias");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Subcategorias",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subcategorias_Categorias_CategoriaId",
+                table: "Subcategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subcategorias_Categorias_CategoriaId",
+                table: "Subcategorias");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Subcategorias",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subcategorias_Categorias_CategoriaId",
+                table: "Subcategorias",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
