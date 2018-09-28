9b848e77f0805b14285a03a274ae033a7f17e58b
Guilherme Francisco <guilherme.francis2017@gmail.com>
2018-09-28 12:25:45 -0300

    Backup

--- T3/gallows/js/sprite.js
+++ T3/gallows/js/sprite.js
@@ -1,17 +1,42 @@
-function createSprite(selector) {
+const createSprite = selector => {
 
-    var elemento = $(selector);
+    const moveFrame = (from, to) => {
 
-    var frames = ['frame1', 'frame2', 'frame3', 'frame4', 'frame5','frame6', 'frame7', 'frame8', 'frame9'];
+        $el.removeClass(from)
+            .addClass(to);
+    };
 
-    var ultimo = frames.length -1; 
-    var atual = 0;
-    var proximo = ++atual;
+    const hasNext = () => current + 1 <= last;
 
-    elemento.addClass(frames[atual]);  
+    const nextFrame = () => {
 
-    function nextFrame() {
+        if (hasNext()) moveFrame(frames[current], frames[++current]);
+    };
 
-        elemento.removeClass(frames[atual]).addClass(frames[proximo]);
-    }
-}
+    const reset = () => {
+
+        moveFrame(frames[current], frames[0]);
+        current = 0;
+    };
+
+    const isFinished = () => !hasNext();
+
+    const $el = $(selector);
+
+    const frames = [
+        'frame1', 'frame2', 'frame3', 'frame4', 'frame5',
+        'frame6', 'frame7', 'frame8', 'frame9'
+    ];
+
+    let current = 0;
+    const last = frames.length - 1;
+
+    $el.addClass(frames[current]);
+
+    return {
+        nextFrame,
+        reset,
+        isFinished
+    };
+};
+
